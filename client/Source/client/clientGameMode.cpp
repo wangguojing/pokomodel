da94befef0eb3c53e2c95bb3fe56914ab91cf07d
wangguojing <nalune@163.com>
2016-08-26 14:08:30 +0800

    commit ue project.

---
+++ client/Source/client/clientGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "client.h"
+#include "clientGameMode.h"
+
+
+
+
---
+++ client/Source/client/clientGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "clientGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIENT_API AclientGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
